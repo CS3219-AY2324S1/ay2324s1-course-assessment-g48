8e46163b28ef7001524f329f5b386778208fd52e
douglch <douglchwm@gmail.com>
2023-09-06 18:05:39 +0800

    Add middleware for api logging purposes

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -5,6 +5,8 @@ const app = express();
 // Dictates default port and URI
 const config = require('./utils/config')
 
+const middleware = require('./utils/middleware')
+
 const logger = require('./utils/logger')
 
 const mongoose = require('mongoose')
@@ -19,5 +21,11 @@ mongoose.connect(config.MONGODB_URI)
         logger.error('error connection to MongoDB:', error.message)
     })
 
+app.use(express.json())
+
+app.use(middleware.requestLogger)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+    
 module.exports = app
 
