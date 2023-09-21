2fe83d19fd09e4eec5a3f38ffcf8ad40c778669d
Huang Hao <huang_hao12@hotmail.com>
2023-09-21 13:50:52 +0800

    Change template for matching page

--- apps/frontend/src/pages/matching.tsx
+++ apps/frontend/src/pages/matching.tsx
@@ -14,108 +14,33 @@ const MatchingPage: React.FC<matchingProps> = () => {
   return (
     <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
       <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-        <div>
-          <label
-            htmlFor="first-name"
-            className="block text-sm font-semibold leading-6 text-gray-900"
-          >
-            First name
-          </label>
-          <div className="mt-2.5">
-            <input
-              type="text"
-              name="first-name"
-              id="first-name"
-              autoComplete="given-name"
-              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
-        <div>
-          <label
-            htmlFor="last-name"
-            className="block text-sm font-semibold leading-6 text-gray-900"
-          >
-            Last name
-          </label>
-          <div className="mt-2.5">
-            <input
-              type="text"
-              name="last-name"
-              id="last-name"
-              autoComplete="family-name"
-              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
         <div className="sm:col-span-2">
           <label
-            htmlFor="company"
+            htmlFor="language"
             className="block text-sm font-semibold leading-6 text-gray-900"
           >
-            Company
-          </label>
-          <div className="mt-2.5">
-            <input
-              type="text"
-              name="company"
-              id="company"
-              autoComplete="organization"
-              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
-        <div className="sm:col-span-2">
-          <label
-            htmlFor="email"
-            className="block text-sm font-semibold leading-6 text-gray-900"
-          >
-            Email
-          </label>
-          <div className="mt-2.5">
-            <input
-              type="email"
-              name="email"
-              id="email"
-              autoComplete="email"
-              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
-        <div className="sm:col-span-2">
-          <label
-            htmlFor="phone-number"
-            className="block text-sm font-semibold leading-6 text-gray-900"
-          >
-            Phone number
+            Language
           </label>
           <div className="relative mt-2.5">
-            <div className="absolute inset-y-0 left-0 flex items-center">
-              <label htmlFor="country" className="sr-only">
-                Country
-              </label>
-              <select
-                id="country"
-                name="country"
+            <input
+              type="tel"
+              name="phone-number"
+              id="phone-number"
+              autoComplete="tel"
+              className="block w-full rounded-md border-0 px-3.5 py-2  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+          <div className="relative mt-2.5">
+          <select
+                id="difficulty"
+                name="difficulty"
                 className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
               >
                 <option>US</option>
                 <option>CA</option>
                 <option>EU</option>
               </select>
-              <ChevronDownIcon
-                className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
-                aria-hidden="true"
-              />
-            </div>
-            <input
-              type="tel"
-              name="phone-number"
-              id="phone-number"
-              autoComplete="tel"
-              className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
+              </div>
         </div>
         <div className="sm:col-span-2">
           <label
@@ -168,7 +93,7 @@ const MatchingPage: React.FC<matchingProps> = () => {
           type="submit"
           className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
-          Let's talk
+          Match
         </button>
       </div>
     </form>
