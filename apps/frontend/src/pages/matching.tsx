f07f9eb7227f5b88634a69a52000983c1eb58a52
Huang Hao <huang_hao12@hotmail.com>
2023-09-22 18:14:36 +0800

    Update the template

--- apps/frontend/src/pages/matching.tsx
+++ apps/frontend/src/pages/matching.tsx
@@ -14,7 +14,7 @@ const MatchingPage: React.FC<matchingProps> = () => {
   const [agreed, setAgreed] = useState(false);
 
   return (
-    <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+    <form action="#" method="POST" className="self-center mt-16 max-w-xl sm:mt-20">
       <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
         <div className="sm:col-span-2">
           <label
@@ -71,34 +71,6 @@ const MatchingPage: React.FC<matchingProps> = () => {
             />
           </div>
         </div>
-        <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
-          <div className="flex h-6 items-center">
-            <Switch
-              checked={agreed}
-              onChange={setAgreed}
-              className={classNames(
-                agreed ? "bg-indigo-600" : "bg-gray-200",
-                "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              )}
-            >
-              <span className="sr-only">Agree to policies</span>
-              <span
-                aria-hidden="true"
-                className={classNames(
-                  agreed ? "translate-x-3.5" : "translate-x-0",
-                  "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"
-                )}
-              />
-            </Switch>
-          </div>
-          <Switch.Label className="text-sm leading-6 text-gray-600">
-            By selecting this, you agree to our{" "}
-            <a href="#" className="font-semibold text-indigo-600">
-              privacy&nbsp;policy
-            </a>
-            .
-          </Switch.Label>
-        </Switch.Group>
       </div>
       <div className="mt-10">
         <button
