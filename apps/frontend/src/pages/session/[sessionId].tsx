b70450f3bd73dee6bde0af2ae866dcb00326adb9
igezt <pierce.nws@gmail.com>
2023-10-18 00:15:19 +0800

    Add new SessionQuestionWorkspace component. As well as a concurrently way of running all the servers at once.

--- apps/frontend/src/pages/session/[sessionId].tsx
+++ apps/frontend/src/pages/session/[sessionId].tsx
@@ -2,47 +2,24 @@ import { AutomergeUrl } from "@automerge/automerge-repo";
 import { useDocument } from "@automerge/automerge-repo-react-hooks";
 import { useRouter } from "next/router";
 import { ChangeEventHandler, useEffect, useRef, useState } from "react";
-import { next as A, change } from "@automerge/automerge";
 import { Doc } from "@/utils/doc";
 import axios from "../api/axios/axios";
 import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
-import { editor } from "monaco-editor";
+import { useTheme } from "@/hook/ThemeContext";
+import SessionCodeEditor from "@/components/questions/questionPage/codeEditor/SessionCodeEditor";
+import SessionQuestionWorkspace from "@/components/questions/questionPage/SessionQuestionWorkspace";
+import useQuestionById from "@/hook/useQuestionById";
 
 export default function Session() {
   const sessionID = useRouter().query.sessionId as string;
-
-  const [docUrl, setDocUrl] = useState<AutomergeUrl>();
-
-  useEffect(() => {
-    axios
-      .get(
-        `${process.env.NEXT_PUBLIC_SESSION_URL}/session/get-session/${sessionID}`
-      )
-      .then((res) => {
-        console.log(res.data.docId);
-        setDocUrl(res.data.docId);
-        return;
-      });
-  }, []);
-
-  const [doc, changeDoc] = useDocument<Doc>(docUrl);
-
-  const increment = (value: any, event: any) => {
-    changeDoc((d) => (d.text = value));
-  };
-
-  const changeText: ChangeEventHandler<HTMLInputElement> = (event) => {
-    changeDoc((d) => (d.text = event.target.value));
-  };
+  const questionId = "6509aea00cbd6c2179ad44d2";
+  const { question, isLoading, error } = useQuestionById(questionId);
 
   return (
     <div>
-      <Editor
-        height="90vh"
-        onChange={increment}
-        value={doc?.text}
-        defaultLanguage="javascript"
-      />
+      {question && (
+        <SessionQuestionWorkspace question={question} sessionId={sessionID} />
+      )}
     </div>
   );
 }
