d043149db368261c169a24b79af65081b8c46708
Huang Hao <huang_hao12@hotmail.com>
2023-10-20 00:44:10 +0800

    Add update button

--- apps/frontend/src/pages/questions/index.tsx
+++ apps/frontend/src/pages/questions/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { PlusIcon } from "@heroicons/react/20/solid";
+import { PlusIcon, ArrowPathIcon } from "@heroicons/react/20/solid";
 import useQuestion from "@/hook/useQuestions";
 import LoadingModal from "@/components/LoadingModal";
 import useSessionUser from "@/hook/useSessionUser";
@@ -25,7 +25,17 @@ export default function QuestionsRepo() {
             It&apos;s grinding time!
           </h1>
           {userRole === Role.Admin && (
-            <span className="sm:ml-3" hidden={isLoading}>
+            <span className="sm:ml-3 space-x-2" hidden={isLoading}>
+              <button
+                type="button"
+                className="inline-flex items-center rounded-md bg-purple-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                <ArrowPathIcon
+                  className="-ml-0.5 mr-1.5 h-5 w-5"
+                  aria-hidden="true"
+                />
+                Update
+              </button>
               <button
                 type="button"
                 className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
