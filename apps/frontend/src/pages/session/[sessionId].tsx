02fe6d933dc33a3432bef348b1d57dc288d52696
Jarrett Teo <jarrett0203@gmail.com>
2023-10-22 14:33:45 +0800

    fix session unauthorised

--- apps/frontend/src/pages/session/[sessionId].tsx
+++ apps/frontend/src/pages/session/[sessionId].tsx
@@ -1,11 +1,19 @@
 import { useRouter } from "next/router";
 import SessionQuestionWorkspace from "@/components/questions/questionPage/SessionQuestionWorkspace";
 import useQuestionById from "@/hook/useQuestionById";
+import { useEffect, useState } from "react";
+import useSessionUser from "@/hook/useSessionUser";
 
 export default function Session() {
+  const { sessionUser } = useSessionUser();
+  const [userRole, setUserRole] = useState(sessionUser == null ? null : sessionUser?.role);
   const sessionID = useRouter().query.sessionId as string;
   const questionId = "6509aea00cbd6c2179ad44d2";
-  const { question } = useQuestionById(questionId);
+  const { question } = useQuestionById(questionId, userRole);
+  
+  useEffect(() => {
+    setUserRole(sessionUser == null ? null : sessionUser?.role);
+  }, [sessionUser]);
 
   return (
     <div>
