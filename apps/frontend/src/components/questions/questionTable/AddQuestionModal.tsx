4143e0aa52e64a57a7da63f1b0c37fff91cbe086
Huang Hao <huang_hao12@hotmail.com>
2023-10-27 15:24:37 +0800

    Save

--- apps/frontend/src/components/questions/questionTable/AddQuestionModal.tsx
+++ apps/frontend/src/components/questions/questionTable/AddQuestionModal.tsx
@@ -347,7 +347,6 @@ const AddQuestionModal: React.FC<AddQuestionModalProps> = ({
                           <input
                             className="ml-2 block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 focus:ring-0 sm:text-sm sm:leading-6"
                             type="text"
-                            name="input"
                             value={testcase.input}
                             onChange={(e) => handleInputChange(index, e.target.value)}
                           />
@@ -368,7 +367,6 @@ const AddQuestionModal: React.FC<AddQuestionModalProps> = ({
                           <input
                             className="ml-2 block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 focus:ring-0 sm:text-sm sm:leading-6"
                             type="text"
-                            name="output"
                             value={testcase.output}
                             onChange={(e) => handleOutputChange(index, e.target.value)}
                           />
