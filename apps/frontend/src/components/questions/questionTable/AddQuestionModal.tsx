5757f75ad67d5714e9dac41f7422f06524a28d4b
Jarrett Teo <jarrett0203@gmail.com>
2023-10-30 21:25:41 +0800

    abstract out title from addQuestionModal

--- apps/frontend/src/components/questions/questionTable/AddQuestionModal.tsx
+++ apps/frontend/src/components/questions/questionTable/AddQuestionModal.tsx
@@ -13,6 +13,7 @@ import Modal from "../../Modal";
 import remarkMath from "remark-math";
 import { useError } from "@/hook/ErrorContext";
 import TestCases from "./modalParts/TestCases";
+import TitleInput from "./modalParts/TitleInput";
 
 
 type AddQuestionModalProps = {
@@ -83,43 +84,14 @@ const AddQuestionModal: React.FC<AddQuestionModalProps> = ({
         <form onSubmit={handleAddQuestion}>
           <div className="space-y-12">
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-              <div className="col-span-full">
-                <legend className="block text-sm font-semibold leading-6 text-gray-900">
-                  Title
-                </legend>
-                <div className="mt-2">
-                  <div
-                    className={`flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset ${
-                      hasError
-                        ? "focus-within:ring-red-600"
-                        : "focus-within:ring-indigo-600"
-                    } sm:max-w-full`}
-                  >
-                    <input
-                      type="text"
-                      name="title"
-                      id="title"
-                      className="block ml-2 flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 focus:ring-0 sm:text-sm sm:leading-6"
-                      value={value}
-                      onChange={(
-                        event: React.ChangeEvent<HTMLInputElement>
-                      ) => {
-                        valueChangeHandler(event);
-                        setNewQuestion({
-                          ...newQuestion,
-                          title: event.target.value,
-                        });
-                      }}
-                      onBlur={inputBlurHandler}
-                    />
-                  </div>
-                  {hasError && value.trim().length <= 0 ? (
-                    <label className="mt-1 text-sm leading-6 text-red-600">
-                      Input cannot be empty.
-                    </label>
-                  ) : null}
-                </div>
-              </div>
+              <TitleInput
+                hasError={hasError}
+                value={value}
+                valueChangeHandler={valueChangeHandler}
+                inputBlurHandler={inputBlurHandler}
+                newQuestion={newQuestion}
+                setNewQuestion={setNewQuestion}
+              />
 
               <div className="col-span-full">
                 <legend className="block text-sm font-semibold leading-6 text-gray-900">
