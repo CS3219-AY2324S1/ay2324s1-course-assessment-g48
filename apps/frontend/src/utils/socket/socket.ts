7172084f1339b2e179564c3acb7903836ba600a6
Guo-KeCheng <guo.kecheng99@gmail.com>
2023-11-02 20:58:19 +0800

    remove websocket for deployment

--- apps/frontend/src/utils/socket/socket.ts
+++ apps/frontend/src/utils/socket/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 export const matchingSocket = io(String(process.env.NEXT_PUBLIC_WS_URL), {
-  transports: ["websocket"],
+  // transports: ["websocket"],
   path: "/queue",
 });
 
