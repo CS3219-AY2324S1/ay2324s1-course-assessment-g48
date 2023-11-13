6850b5854e255d7eec941c11d5e41c67619609b2
Guo-KeCheng <guo.kecheng99@gmail.com>
2023-11-14 01:05:01 +0800

    Working on leetpal

--- apps/frontend/src/utils/socket/socket.ts
+++ apps/frontend/src/utils/socket/socket.ts
@@ -2,8 +2,10 @@ import { io } from "socket.io-client";
 
 export const matchingSocket = io(String(process.env.NEXT_PUBLIC_WS_URL), {
   path: "/queue",
+  upgrade: false,
 });
 
 export const chatroomSocket = io(String(process.env.NEXT_PUBLIC_CHAT_URL), {
   path: "/chat",
+  upgrade: false,
 });
